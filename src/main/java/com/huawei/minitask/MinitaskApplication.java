67b5b581e9eb6c4459070b0480d7a67717b5b725
zsjwish <zsjwish@126.com>
2019-08-11 23:55:40 +0800

    mybatis init

---
+++ src/main/java/com/huawei/minitask/MinitaskApplication.java
@@ -0,0 +1,13 @@
+package com.huawei.minitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinitaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinitaskApplication.class, args);
+	}
+
+}
